97cd2eda9296e66e8147bb4ccd7603f5d65f999c
unknown <alitarek868@yahoo.com>
2024-03-03 03:16:40 +0200

    finish responsive

--- src/app/components/citadel/citadel.component.ts
+++ src/app/components/citadel/citadel.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-citadel',
   templateUrl: './citadel.component.html',
-  styleUrls: ['./citadel.component.scss']
+  styleUrls: ['./citadel.component.scss'],
 })
 export class CitadelComponent {
+  isLargeScreen: boolean = false;
+  constructor() {
+    this.checkScreenSize();
+  }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    // Define your specific page width threshold here
+    const widthThreshold = 928;
+    this.isLargeScreen = window.innerWidth > widthThreshold;
+  }
 }
--- src/app/components/projects-parties/projects-parties.component.ts
+++ src/app/components/projects-parties/projects-parties.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-projects-parties',
   templateUrl: './projects-parties.component.html',
-  styleUrls: ['./projects-parties.component.scss']
+  styleUrls: ['./projects-parties.component.scss'],
 })
 export class ProjectsPartiesComponent {
+  isLargeScreen: boolean = false;
 
+  constructor() {
+    this.checkScreenSize();
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event?: any) {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    // Define your specific page width threshold here
+    const widthThreshold = 992;
+    this.isLargeScreen = window.innerWidth > widthThreshold;
+  }
 }
